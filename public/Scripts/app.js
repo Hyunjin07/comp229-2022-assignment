f5c8e37ce16991761e65e2e105582ed1ab26202a
Hyunjin07 <111410288+Hyunjin07@users.noreply.github.com>
2022-10-02 03:26:21 -0400

    initial commit

---
+++ public/Scripts/app.js
@@ -0,0 +1,15 @@
+/*Name: Hyujin Park
+Student ID: 301157840
+Date: 2022/10/02  
+*/ 
+//IIFE -- Immediately Invoked Function Expression
+(function(){
+    function Start(){
+        console.log("App Started...");
+    }
+    window.addEventListener("load",Start);
+    
+})();
+
+
+
---
+++ routes/index.js
@@ -0,0 +1,51 @@
+const { response } = require('express');
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Home' });
+});
+
+/* GET home page. */
+router.get('/home', function(req, res, next) {
+  res.render('index', { title: 'Home' });
+});
+
+/* GET About page. */
+router.get('/about', function(req, res, next) {
+  res.render('index', { title: 'About'});
+});
+
+/* GET Products page. */
+router.get('/projects', function(req, res, next) {
+  res.render('index', { title: 'Project' });
+});
+
+/* GET Services page. */
+router.get('/services', function(req, res, next) {
+  res.render('index', { title: 'Services' });
+});
+
+/* GET Contact Us page. */
+router.get('/contact', function(req, res, next) {
+  res.render('index', { title: 'Contact' });
+});
+
+/*redircet from contact page to home page*/ 
+router.post("/contact", function (req, res) {
+  res.redirect("/");
+  response = {
+    first_name: req.query.first_name,
+    last_name: req.query.last_name,
+    email: req.query.email,
+    contact: req.query.contact
+  };
+  console.log(response);
+  res.end(JSON.stringify(response));
+ 
+});
+
+
+
+module.exports = router;
